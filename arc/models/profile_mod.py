f4559dffac5bc0567407eb2226b7e0f2d9a087a6
Anshuman Singh Airy <anshuman.airy04@gmail.com>
2019-08-26 16:35:43 +0530

    4:35 PM

--- arc/models/profile_mod.py
+++ arc/models/profile_mod.py
@@ -3,7 +3,4 @@ from django.contrib.auth.models import User
 
 class display_picture(models.Model):
     profile_picture = models.ImageField(upload_to='Profile', blank=True)
-    idx = models.IntegerField()
-
-    def __str__(self):
-        return user_id
+    idx1 = models.IntegerField()
