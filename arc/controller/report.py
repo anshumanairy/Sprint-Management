c52f0b30d4cc8fb3ae49949d35f786a906572fa7
Anshuman Airy <anshuman.airy@quikr.com>
2019-08-29 16:23:39 +0530

    Initial Commit

--- arc/controller/report.py
+++ arc/controller/report.py
@@ -40,7 +40,6 @@ from django.contrib.auth.models import Permission
 from django.utils.timezone import utc
 from django.utils import timezone
 
-
 @login_required(login_url='/')
 def tasks(request):
     try:
